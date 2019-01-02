ae644a4c9fc0b29bf8c48c728b17af92907fb262
jazzyshi <405616092@qq.com>
2019-01-02 10:52:20 +0800

    test

--- sysapi/src/main/java/com/weifuwu/sysapi/SysService.java
+++ sysapi/src/main/java/com/weifuwu/sysapi/SysService.java
@@ -4,7 +4,7 @@ package com.weifuwu.sysapi;
  * Created by jzshi on 2018/12/21.
  */
 public interface SysService {
-    String getPersonInfo();
+    String getPersonInfo1();
 
     String testlocal();
 
